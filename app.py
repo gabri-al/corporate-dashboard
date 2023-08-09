762939b44df42c129904003b53ce637707e783f9
Kristin McClinton <kristin.mcclinton@cummins.com>
2023-08-09 16:59:55 -0500

    dashboard

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import dash
+
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
+
+# POC
+# https://adb-8825802256197292.12.azuredatabricks.net/?o=8825802256197292#
+# https://adb-8825802256197292.12.azuredatabricks.net/explore/data/npbu/bevtelematics_primary/accelera_master_e_hb?o=8825802256197292
+# https://adb-8825802256197292.12.azuredatabricks.net/sql/warehouses/d689c0abce68dd88?o=8825802256197292#
